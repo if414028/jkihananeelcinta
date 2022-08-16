27f311c490bf2116e6e92506a92523d88a231a63
Reynaldo Napitupulu <reynaldo.napitupulu@advotics.com>
2022-08-16 19:27:35 +0700

    Initial commit

---
+++ app/src/main/java/com/jki/hananeelcinta/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.jki.hananeelcinta.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jki.hananeelcinta.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
