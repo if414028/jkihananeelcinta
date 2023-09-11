258a767ff756139dc379a43e5662a2bb7866890f
reynaldo <voldarex.napitupulu@gmail.com>
2023-09-11 09:11:15 +0700

    initiate pastor message list rec view

--- app/src/main/java/com/jki/hananeelcinta/pastoral/pastormessages/PastorMessagesListActivity.kt
+++ app/src/main/java/com/jki/hananeelcinta/pastoral/pastormessages/PastorMessagesListActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jki.hananeelcinta.R
 import com.jki.hananeelcinta.databinding.ActivityPastorMessagesListBinding
+import com.jki.hananeelcinta.databinding.ItemPastorMessageBinding
 import com.jki.hananeelcinta.model.PastorMessage
 import com.jki.hananeelcinta.util.SimpleRecyclerAdapter
 
@@ -19,6 +22,7 @@ class PastorMessagesListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_pastor_messages_list)
+        supportActionBar?.hide()
         setupLayout()
     }
 
@@ -27,9 +31,19 @@ class PastorMessagesListActivity : AppCompatActivity() {
             val intent = Intent(this, CreatePastorMessagesActivity::class.java)
             startActivity(intent)
         }
+
+        initRecyclerView()
     }
 
     private fun initRecyclerView() {
-
+        binding.rvPastorMessage.layoutManager =
+            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        adapter = SimpleRecyclerAdapter(
+            arrayListOf(),
+            R.layout.item_pastor_message
+        ) { holder, item ->
+            val itemBinding: ItemPastorMessageBinding =
+                holder?.layoutBinding as ItemPastorMessageBinding
+        }
     }
 }
