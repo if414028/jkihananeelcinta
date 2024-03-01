305981bc97c8c5384415696229eecf8b5b4da7fe
Reynaldo Napitupulu <reynaldo.napitupulu@advotics.com>
2024-03-01 17:11:42 +0700

    [x] create forgot password

--- app/src/main/java/com/jki/hananeelcinta/login/LoginActivity.kt
+++ app/src/main/java/com/jki/hananeelcinta/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.jki.hananeelcinta.login
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.text.InputType
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,12 +15,14 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.jki.hananeelcinta.home.MainActivity
 import com.jki.hananeelcinta.R
+import com.jki.hananeelcinta.R.*
 import com.jki.hananeelcinta.databinding.ActivityLoginBinding
+import com.jki.hananeelcinta.home.MainActivity
 import com.jki.hananeelcinta.model.User
 import com.jki.hananeelcinta.register.RegisterActivity
 import com.jki.hananeelcinta.util.UserConfiguration
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -46,6 +49,26 @@ class LoginActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             signIn()
         }
+
+        binding.tvForgotPassword.setOnClickListener {
+            val intent = Intent(applicationContext, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnTogglePassword.setOnClickListener { togglePassword() }
+    }
+
+    private fun togglePassword() {
+        val inputType = binding.etPassword.inputType
+        //129 == input type textPassword, not registered in InputType class
+        if (inputType == 129) {
+            binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT
+            binding.btnTogglePassword.setImageResource((drawable.ic_eye_open))
+        } else {
+            binding.etPassword.inputType = 129
+            binding.btnTogglePassword.setImageResource(R.drawable.ic_eye_closed)
+        }
+        binding.etPassword.setSelection(binding.etPassword.length());
     }
 
     private fun showLoading(isVisible: Boolean) {
